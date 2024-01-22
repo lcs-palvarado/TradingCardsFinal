2caedbc841dc222b95c50de953d7612c7476c0f6
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-22 12:30:19 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Pablo Alvarado on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
