618d706596aff4981218061034247f7d2f10e3e6
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-23 14:54:01 -0500

    Tried to add text in the middle top but failed.

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -9,66 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Color.black
+            .scaledToFit()
+            .frame(width: 420)
+        VStack{
+            
+            Image("Tatis")
+                .resizable()
+                .scaledToFit()
+                .clipShape(
+                    Ellipse()
+                )
+                .overlay {
                     VStack{
                         
-                        Image("Tatis")
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(
-                                Ellipse()
-                            )
-                            .overlay {
-                                VStack{
-                                    
-                                    //Top
-                                    HStack{
-                                        Text("Top Left")
-                                            .foregroundStyle(.white)
-                                            .padding(.horizontal)
-                                        
-                                        Spacer()
-                                        
-                                        Text("Top Right")
-                                            .foregroundStyle(.white)
-                                            .padding(.horizontal)
-                                    }
-                                    
-                                    Spacer()
-                                    
-                                    //Bottom
-                                    HStack{
-                                        VStack {
-                                            Text("#23")
-                                                .foregroundStyle(.gray)
-                                                .padding(.horizontal)
-                                            
-                                            Text("Tatis Jr")
-                                                .foregroundStyle(.white)
-                                        }
-                                        
-                                        Spacer()
-                                        
-                                        VStack {
-                                            Image("Padres")
-                                                .resizable()
-                                                .scaledToFit()
-                                                .clipShape(Circle())
-                                                .frame(width: 75, height: 75)
-                                            
-                                            
-                                            Text("Bottom Right")
-                                                .foregroundStyle(.white)
-                                                .padding(.horizontal)
-                                        }
-                                    }
-                                }
+                        //Top
+                        HStack{
+                            Text("Top Left")
+                                .foregroundStyle(.white)
+                                .padding(.horizontal)
+                            
+                            Spacer()
+                            
+                            
+                            Text("Top Right")
+                                .foregroundStyle(.white)
+                                .padding(.horizontal)
+                        }
+                        
+                        Spacer()
+                        
+                        //Bottom
+                        HStack{
+                            VStack {
+                                Text("#23")
+                                    .foregroundStyle(.gray)
+                                    .padding(.horizontal)
+                                
+                                Text("Tatis Jr")
+                                    .foregroundStyle(.white)
                             }
-                       
+                            
+                            Spacer()
+                            
+                            VStack {
+                                Image("Padres")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .clipShape(Circle())
+                                    .frame(width: 75, height: 75)
+                                
+                                
+                                Text("Bottom Right")
+                                    .foregroundStyle(.white)
+                                    .padding(.horizontal)
+                            }
+                        }
                     }
-                    .frame(width: 400, height: 600)
-
-            }
+                }
+            
         }
+        .frame(width: 400, height: 600)
+        
+    }
+}
     
 
 
