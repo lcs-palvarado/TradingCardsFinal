316635c3a7830a91c18d4193447c628fe41d4acd
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-25 20:06:23 -0500

    Not sure what to do. For some reason, my list is not visible: I can't see the list, title , or destination.

--- TradingCards/ListView.swift
+++ TradingCards/ListView.swift
@@ -12,24 +12,29 @@ struct ListView: View {
         
         NavigationStack{
             List{
-               
+                
+                NavigationLink(destination: tatisJr){
+                    ListExample(playerIcon: "Tatis", playerName: "Fernando Tatis Jr", nationality: "Dominican Republic")}
+                
                 NavigationLink(destination: judge){
-                    ListExample(playerIcon: "Tatis", playerName: "Fernando Tatis Jr", nationality: "Dominican Republic")
-                    
-                    ListExample(playerIcon: "Judge", playerName: "Aaron Judge", nationality: "United States of America")
-                    
-                    ListExample(playerIcon: "Freeman", playerName: "Freddie Freeman", nationality: "United States of America")
-                    
-                    ListExample(playerIcon: "Trout", playerName: "Mike Trout", nationality: "United States of America")
-                    
-                    ListExample(playerIcon: "Yordan", playerName: "Yordan Alvarez", nationality: "Cuba")
-                }
+                    ListExample(playerIcon: "Judge", playerName: "Aaron Judge", nationality: "United States of America")}
+                
+                NavigationLink(destination: freeman){
+                    ListExample(playerIcon: "Freeman", playerName: "Freddie Freeman", nationality: "United States of America")}
+                
+                NavigationLink(destination: trout){
+                    ListExample(playerIcon: "Trout", playerName: "Mike Trout", nationality: "United States of America")}
+                
+                NavigationLink(destination: alvarez){
+                    ListExample(playerIcon: "Yordan", playerName: "Yordan Alvarez", nationality: "Cuba")}
             }
         }
-        
+        .navigationTitle("My Favorite Players")
+        .frame(width: 400, height: 600)
+    }
         
     }
-}
+
 
 #Preview {
     ListView()
